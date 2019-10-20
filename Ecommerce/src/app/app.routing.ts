8e1e966432c918a2414f0d15d409e65af6d5b437
pmore23 <23priyankamore@gmail.com>
2019-10-20 14:35:12 +0530

    added order history route in the application

--- Ecommerce/src/app/app.routing.ts
+++ Ecommerce/src/app/app.routing.ts
@@ -1,12 +1,14 @@
 import { Routes, RouterModule } from '@angular/router';
 import { CartComponent } from './cart/cart.component';
 import { ProductComponent } from './product/product.component';
+import { OrderHistoryComponent } from './order/orderHistory.component';
 
 
 const routes: Routes = [
 	{ path: '', component: ProductComponent },
 	{ path: 'products', component: ProductComponent },
 	{ path: 'cart', component: CartComponent },
+	{ path: 'orderHistory', component: OrderHistoryComponent },
 	{ path: '**', redirectTo: '' }
 ];
 
