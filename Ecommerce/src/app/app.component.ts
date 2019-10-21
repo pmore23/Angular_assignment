4bcda4d9437f1cf6dd50bdab137735054d1eb14d
pmore23 <23priyankamore@gmail.com>
2019-10-21 22:16:41 +0530

    localstorage cleared on page refresh in the application

--- Ecommerce/src/app/app.component.ts
+++ Ecommerce/src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit, Output, EventEmitter } from '@angular/core';
+import { Component, OnInit, Output, EventEmitter, Inject } from '@angular/core';
+import {LOCAL_STORAGE, WebStorageService} from 'angular-webstorage-service';
 
 @Component({
   selector: 'app-root',
@@ -9,8 +10,11 @@ export class AppComponent implements OnInit{
   title = 'Ecommerce Application';
   @Output() public sidenavToggle = new EventEmitter();
  
-  constructor() { }
+  constructor(
+    @Inject(LOCAL_STORAGE) private storage: WebStorageService
+  ) { }
   ngOnInit() {
+    this.storage.remove('orderHistory');
   }
   public onToggleSidenav = () => {
     this.sidenavToggle.emit();
