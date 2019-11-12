aab11d9cbd37e36cf54da166d14fd999ba60e47f
pmore23 <23priyankamore@gmail.com>
2019-11-12 23:04:13 +0530

    test cases for app component executed successfully

--- Ecommerce/src/app/in-memory-api/app-in-memory-api.service.spec.ts
+++ Ecommerce/src/app/in-memory-api/app-in-memory-api.service.spec.ts
@@ -5,7 +5,7 @@ import { AppInMemoryApiService } from './app-in-memory-api.service';
 describe('AppInMemoryApiService', () => {
   beforeEach(() => TestBed.configureTestingModule({}));
 
-  it('should be created', () => {
+  it('AppInMemoryApiService should be created', () => {
     const service: AppInMemoryApiService = TestBed.get(AppInMemoryApiService);
     expect(service).toBeTruthy();
   });
